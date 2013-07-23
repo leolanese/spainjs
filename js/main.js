c9cea97ef9c579fb673527baeb394b42e2a13192
Leo Lanese <developer@leolanese.com>
2013-07-23 12:25:11 +0100

    spainjs

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+/**/
+var notHere = ( function () {
+    var canvas;
+    var ctx;
+    var imgData;
+    var pix;
+    var WIDTH;
+    var HEIGHT;
+    var flickerInterval;
+
+    var init = function () {
+        canvas = document.getElementById('bw');
+        ctx = canvas.getContext('2d');
+        canvas.width = WIDTH = 700;
+        canvas.height = HEIGHT = 500;
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+        ctx.fill();
+        imgData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
+        pix = imgData.data;
+        flickerInterval = setInterval(flickering, 30);
+    };
+
+    var flickering = function () {
+        for (var i = 0; i < pix.length; i += 4) {
+            var color = (Math.random() * 255) + 50;
+            pix[i] = color;
+            pix[i + 1] = color;
+            pix[i + 2] = color;
+        }
+        ctx.putImageData(imgData, 0, 0);
+    };
+
+    return {
+        init: init
+    };
+}());
+
+notHere.init();
